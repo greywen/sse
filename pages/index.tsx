d68e2dce4e0077c213de283422f6a95073d147b2
gray <gray.wen@outlook.com>
2025-02-21 11:24:05 +0800

    完成实例一

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className='flex justify-center gap-8 mt-4'>
+      <Link key='one' href='/examples/one'>
+        实例一
+      </Link>
+      <Link key='two' href='/examples/two'>
+        实例二
+      </Link>
+      <Link key='three' href='/examples/three'>
+        实例三
+      </Link>
+      <Link key='four' href='/examples/four'>
+        实例四
+      </Link>
+    </div>
+  );
 }
